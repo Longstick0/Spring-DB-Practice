3390bcea51b33f7684ae06a4506dcebf2d7b0092
Longstick0 <789456jang@naver.com>
2022-06-18 02:12:28 +0900

    AOP를 활용한 트랜잭션 처리 방법 (@Transational)

--- jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_3.java
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -1,24 +1,37 @@
 package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
-import hello.jdbc.repository.MemberRepositoryV1;
-import lombok.RequiredArgsConstructor;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.SQLException;
 
-@RequiredArgsConstructor
-public class MemberServiceV1 {
+/**
+ * 트랜잭션 - @Transactional AOP
+ */
 
-    private final MemberRepositoryV1 memberRepository;
+@Slf4j
+public class MemberServiceV3_3 {
 
+    private final MemberRepositoryV3 memberRepository;
+
+    public MemberServiceV3_3(MemberRepositoryV3 memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        bizLogic(fromId, toId, money);
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
         Member fromMember = memberRepository.findById(fromId);
         Member toMember = memberRepository.findById(toId);
 
         memberRepository.update(fromId, fromMember.getMoney() - money);
         validation(toMember);
         memberRepository.update(toId, toMember.getMoney() + money);
-        //커밋 or 롤백
     }
 
     private void validation(Member toMember) {
